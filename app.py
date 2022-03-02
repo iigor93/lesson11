ec21cba1ca7e5b7cedb63cc3bbbe3326e1619ca8
Igor <iigor93@gmai.com>
2022-03-02 19:00:19 +0300

    html page

---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template
+import utils
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index_view():
+    """Список всех кандидатов"""
+    candidates_list = utils.get_data_from_file()
+    return render_template('list.html', candidates_list=candidates_list)
+
+
+@app.route('/candidate/')
+def candidate_empty():
+    """Страница кандидата без номера"""
+    return 'Выберете номер кандидата'
+
+
+@app.route('/candidate/<string>')
+def candidate_string(string):
+    """Страница кандидата с набором симовлов"""
+    return f'Выберете номер кандидата, а не строку {string}'
+
+
+@app.route('/candidate/<int:uid>/')
+def candidate(uid):
+    """Страница кандидата"""
+    candidate_num = utils.get_len()
+    if uid not in candidate_num:
+        return ('нет кандидата с таким номером')
+    candidate_data = utils.get_candidate(uid)
+    return render_template('single.html', candidate_data=candidate_data)
+
+
+@app.route(('/search/'))
+def candidate_empty_search():
+    """ Если имя пустое"""
+    return 'Введите имя кандидата'
+
+
+@app.route('/search/<string>/')
+def candidate_search_by_name(string):
+    """Страница поиска кандидата по имени"""
+    candidate_data = utils.get_candidates_by_name(string)
+    if len(candidate_data) == 0:
+        return "Нет кандидатов с таким именем"
+    list_len = len(candidate_data)
+    return render_template('search.html', candidate_data=candidate_data, search_num=list_len)
+
+
+@app.route(('/skill/'))
+def candidate_skill_empty_search():
+    """ Если навык пустое"""
+    return 'Введите навык'
+
+
+@app.route('/skill/<string>/')
+def candidate_search_by_skill(string):
+    """Страница поиска кандидата по Навыкам"""
+    candidate_data = utils.get_candidates_by_skill(string)
+    if len(candidate_data) == 0:
+        return "Нет кандидатов с таким Навыком"
+    list_len = len(candidate_data)
+    return render_template('skill.html', candidate_data=candidate_data, search_num=list_len)
---
+++ utils.py
@@ -0,0 +1,57 @@
+import json
+
+
+def get_data_from_file():
+    """загрузка данных из файла"""
+    file_name = 'candidates.json'
+    with open(file_name, 'r', encoding='utf-8') as file:
+        file_data = json.load(file)
+    return file_data
+
+
+def get_len():
+    """Номера кандидатов"""
+    file_data = get_data_from_file()
+    num_list = []
+    for item in file_data:
+        num_list.append(item['id'])
+    return num_list
+
+
+def load_candidates_from_json():
+    """возвращает список всех кандидатов"""
+    file_data = get_data_from_file()
+    return file_data
+
+
+def get_candidate(candidate_id):
+    """ возвращает одного кандидата по его id"""
+    file_data = get_data_from_file()
+    for candidate in file_data:
+        if candidate_id == candidate['id']:
+            return candidate
+    return 'Нет кандидата с таким номером'
+
+
+def get_candidates_by_name(candidate_name):
+    file_data = get_data_from_file()
+    candidate_list = []
+    """возвращает кандидатов по имени"""
+    for candidate in file_data:
+        if candidate_name.strip().lower() in candidate['name'].strip().lower():
+            candidate_list.append(candidate)
+    return candidate_list
+
+
+def get_candidates_by_skill(skill_name):
+    """возвращает кандидатов по навыку"""
+    file_data = get_data_from_file()
+    list_to_return = []
+    for candidate in file_data:
+        skills_list = candidate['skills'].split(',')
+        for item in skills_list:
+            if skill_name.strip().lower() == item.strip().lower():
+                list_to_return.append(candidate)
+
+    return list_to_return
+
